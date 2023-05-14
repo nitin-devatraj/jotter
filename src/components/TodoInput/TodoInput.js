ecbc3937ed1092986789bf90f3ffab5b1985b36f
nitin devatraj <nitindevatraj@protonmail.com>
2023-05-15 01:35:58 +0530

    create TodoItem component

---
+++ src/components/TodoInput/TodoInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const goalInputChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    props.onAddGoal(enteredValue);
+    setEnteredValue("");
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label>Add New Todo</label>
+        <input
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Todo</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
