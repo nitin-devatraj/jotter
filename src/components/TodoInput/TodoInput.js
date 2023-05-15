9a8226a202aadcc12a1c100fdf3f0de44164ab83
nitin devatraj <nitindevatraj@protonmail.com>
2023-05-16 00:46:02 +0530

    rewrite Button & FormControl components using styled components

--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
-
 import Button from "../UI/Button/Button";
-import "./TodoInput.css";
+import { styled } from "styled-components";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.valid ? "white" : "salmon")};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: none;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+    height: 30px;
+    border-radius: 2px;
+    background-color: ${(props) => (props.valid ? "white" : "salmon")};
+  }
+
+  & input:focus {
+    outline: none;
+    background: whitesmoke;
+  }
+`;
 
 const TodoInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -28,10 +55,10 @@ const TodoInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${isValid ? "" : "invalid"}`}>
+      <FormControl valid={isValid}>
         <label>Add New Todo</label>
         <input type="text" value={enteredValue} onChange={inputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Todo</Button>
     </form>
   );
--- src/components/UI/button/Button.js
+++ src/components/UI/button/Button.js
@@ -1,13 +1,34 @@
-import React from "react";
+import { styled } from "styled-components";
 
-import "./Button.css";
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid whitesmoke;
+  color: white;
+  background: #52b788;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 8px 0;
 
-const Button = (props) => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    transform: translateX(4px);
+  }
+
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+`;
+
+// const Button = (props) => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
